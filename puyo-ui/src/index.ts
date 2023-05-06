b14934f70c8e2de24a1cc4a176bbac42d6eb92b2
charon1212 <charons2turquoise@gmail.com>
2023-05-06 09:28:22 +0900

    add

--- puyo-ui/src/index.ts
+++ puyo-ui/src/index.ts
@@ -4,3 +4,4 @@ export * from './puyo-ui/ShowNext';
 export * from './puyo-ui/TokopuyoGame';
 export * from './puyo-ui/TokopuyoGameKeyConfig';
 export * from './puyo-ui/ColorMapping';
+export * from './puyo-ui/useEditablePuyoBoard';
